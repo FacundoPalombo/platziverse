c3ed02f6f969212d37f4de22f9c8110ae61c0b9b
Facundo Palombo <facundo.palombo@gmail.com>
2020-05-22 01:57:33 -0300

    Added implementation for some methods in Agent lib and their tests

--- platziverse-db/__tests__/agent.test.js
+++ platziverse-db/__tests__/agent.test.js
@@ -2,40 +2,71 @@ const setupAgentModel = require("../models/agent");
 const ava = require("ava");
 const proxyquire = require("proxyquire");
 const sinon = require("sinon");
-const agentFixtures = require("./fixtures/agent");
+const agentFixtures = require("./fixtures/agent"),
+  { single } = require("./fixtures/agent");
 
-let db = null;
+// Testing instances
 let sandbox = null;
-let AgentStub = null;
-
-let MetricStub = {
-  belongsTo: sinon.spy(),
-};
+let db = null;
 let config = {
   logging: function () {},
 };
+
+// Helpers
+let _single = { ...single };
+
+//Stubs
+
+let AgentStub = null;
+let MetricStub = {
+  belongsTo: sinon.spy(),
+};
+let uuidArgs = {
+  where: {
+    uuid: single.uuid,
+  },
+};
+
+/* :===---- Hooks ----===: */
 
 ava.beforeEach(async () => {
   sandbox = sinon.createSandbox(config);
   sandbox.spy();
 
+  // Model AgentStub
   AgentStub = {
     hasMany: sandbox.spy(),
   };
+
+  // Model Agent.findOne Stub
+  AgentStub.findOne = sandbox.stub();
+  AgentStub.findOne
+    .withArgs(uuidArgs)
+    .returns(Promise.resolve(agentFixtures.byUuid(single.uuid)));
+
+  // Model Agent.findById Stub
   AgentStub.findById = sandbox.stub();
   AgentStub.findById
     .withArgs(1)
     .returns(Promise.resolve(agentFixtures.byId(1)));
+
+  // Model Agent.update Stub
+  AgentStub.update = sandbox.stub();
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single));
+
   const setupDatabase = proxyquire("../", {
     "./models/agent": () => AgentStub,
     "./models/metric": () => MetricStub,
   });
+
   db = await setupDatabase(config);
 });
 
 ava.afterEach(() => {
   sandbox && sinon.restore();
 });
+
+/* :=== ---- Tests ---- ===: */
 
 ava("GIVEN an Agent", (t) => {
   t.truthy(db.Agent, "THEN Agent service SHOULD exist");
@@ -58,6 +89,9 @@ ava.serial("GIVEN an Setup", (t) => {
     "THEN MetricModel.belongsTo should be called with AgentModel Arguments"
   );
 });
+
+// :=== Methods from Agent ===:
+
 ava.serial("GIVEN an Agent and method findById", async (t) => {
   let agent = await db.Agent.findById(1);
 
@@ -72,5 +106,17 @@ ava.serial("GIVEN an Agent and method findById", async (t) => {
     agent,
     agentFixtures.byId(1),
     "THEN Agent should return the same value in that method"
+  );
+});
+
+ava.serial("GIVEN an Agent and method createOrUpdate", async (t) => {
+  let agent = await db.Agent.createOrUpdate(_single);
+  t.deepEqual(agent, single, "THEN Agent should be the same element");
+  t.true(AgentStub.findOne.called, "THEN findOne should be called on model");
+  t.true(AgentStub.findOne.calledTwice, "THEn findOne should be called twice");
+  t.true(AgentStub.update.calledOnce, "THEN update should be called once");
+  t.true(
+    AgentStub.update.calledWith(_single),
+    "THEN update should be called with the same args"
   );
 });
--- platziverse-db/lib/agent.js
+++ platziverse-db/lib/agent.js
@@ -3,5 +3,36 @@ module.exports = function setupAgent(AgentModel) {
   function findById(id) {
     return AgentModel.findById(id);
   }
-  return { findById };
+
+  function findByUuid(uuid) {
+    return AgentModel.findOne({ where: { uuid } });
+  }
+
+  function findAll() {
+    return AgentModel.findAll();
+  }
+
+  function findConnected() {
+    return AgentModel.findAll({ where: { connected: true } });
+  }
+
+  function findByUsername(username) {
+    return AgentModel.findAll({ where: { username } });
+  }
+
+  async function createOrUpdate(agent) {
+    const query = {
+      where: {
+        uuid: agent.uuid,
+      },
+    };
+    const existingAgent = await AgentModel.findOne(query);
+    if (existingAgent) {
+      const updated = await AgentModel.update(agent, query);
+      return updated ? AgentModel.findOne(query) : existingAgent;
+    }
+    const result = await AgentModel.create(agent);
+    return result.toJSON();
+  }
+  return { findById, createOrUpdate };
 };
